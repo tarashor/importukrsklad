1a16c0695aa6f4acb2ddc600f459d67061e05eda
Taras <taras.horyachko@gmail.com>
2014-11-05 15:58:38 +0200

    limit

---
+++ UkrskladImporter/TrialLimitation.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UkrskladImporter
+{
+    public class TrialLimitation
+    {
+        internal static int GetCounter()
+        {
+            throw new NotImplementedException();
+        }
+
+        public static int MaxCounter { get; set; }
+
+        internal static void SaveCounter(int savesCount)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
